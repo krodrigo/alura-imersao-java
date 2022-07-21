c66aef8ea33ae6e124512a5444b4ef776fa314bd
Kleber <krodrigo@gmail.com>
2022-07-20 23:12:10 -0300

    Criação de extrator para API do IMDB

---
+++ ExtratorDeConteudoDoImdb.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ExtratorDeConteudoDoImdb {
+  public List<Conteudo> extraiConteudos(String json) {
+    var parser = new JsonParser();
+    List<Map<String, String>> listaDeAtributos = parser.parse((json));
+    List<Conteudo> conteudos = new ArrayList<>();
+
+    for (var item : listaDeAtributos.subList(0, 2)) {
+      String titulo = item.get("title");
+      String urlImagem = item.get("image").replaceAll("(@+)(.*).jpg$", "$1.jpg");
+
+      conteudos.add(new Conteudo(titulo, urlImagem));
+    }
+
+    return conteudos;
+  }
+}
